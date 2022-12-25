b38b2a577123e5c36b4912b7ec68db2222617eca
hungle311994 <115997521+hungle311994@users.noreply.github.com>
2022-12-25 16:05:53 +0700

    responsive

--- src/components/navbar/NavBar.js
+++ src/components/navbar/NavBar.js
@@ -1,90 +1,120 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import "./NavBar.css";
 import { Link } from "react-scroll";
 
 const Navbar = () => {
-  const [isActive, setIsActive] = useState("");
+  const [isActiveLink, setIsActiveLink] = useState("");
+  const [isDropdowns, setIsDropdowns] = useState(true);
 
-  const onHandleActive = (e) => {
-    setIsActive(e);
+  const handleActive = (e) => {
+    setIsActiveLink(e);
   };
+  const handleDropdowns = () => {
+    setIsDropdowns(!isDropdowns);
+  };
+
+  useEffect(() => {
+    document.addEventListener("mousedown", () => setIsDropdowns(true));
+  }, []);
 
   return (
     <div className="nav-container">
       <nav>
         <div className="nav-logo">PORTFOLIO</div>
 
-        <ul className="nav-items">
-          <li>
-            <Link
-              className={isActive === "home" ? "nav-item active" : "nav-item"}
-              to="/home"
-              smooth={true}
-              onClick={() => onHandleActive("home")}
-            >
-              Home
-            </Link>
-          </li>
-          <li>
-            <Link
-              className={isActive === "skills" ? "nav-item active" : "nav-item"}
-              to="/skills"
-              smooth={true}
-              onClick={() => onHandleActive("skills")}
-            >
-              Skills
-            </Link>
-          </li>
-          <li>
-            <Link
-              className={
-                isActive === "projects" ? "nav-item active" : "nav-item"
-              }
-              to="/projects"
-              smooth={true}
-              onClick={() => onHandleActive("projects")}
-            >
-              Projects
-            </Link>
-          </li>
-          <li>
-            <Link
-              className={
-                isActive === "contact" ? "nav-item active" : "nav-item"
-              }
-              to="/contact"
-              smooth={true}
-              onClick={() => onHandleActive("contact")}
-            >
-              Contact
-            </Link>
-          </li>
-        </ul>
+        <div className={`nav-direct ${isDropdowns ? "inactive" : "active"}`}>
+          <ul className="nav-items">
+            <li>
+              <Link
+                className={
+                  isActiveLink === "home" ? "nav-item active" : "nav-item"
+                }
+                to="/home"
+                smooth={true}
+                onClick={() => handleActive("home")}
+              >
+                Home
+              </Link>
+            </li>
+            <li>
+              <Link
+                className={
+                  isActiveLink === "skills" ? "nav-item active" : "nav-item"
+                }
+                to="/skills"
+                smooth={true}
+                onClick={() => handleActive("skills")}
+              >
+                Skills
+              </Link>
+            </li>
+            <li>
+              <Link
+                className={
+                  isActiveLink === "projects" ? "nav-item active" : "nav-item"
+                }
+                to="/projects"
+                smooth={true}
+                onClick={() => handleActive("projects")}
+              >
+                Projects
+              </Link>
+            </li>
+            <li>
+              <Link
+                className={
+                  isActiveLink === "contact" ? "nav-item active" : "nav-item"
+                }
+                to="/contact"
+                smooth={true}
+                onClick={() => handleActive("contact")}
+              >
+                Contact
+              </Link>
+            </li>
+          </ul>
 
-        <div className="nav-links">
-          <a
-            href="https://www.linkedin.com/in/h%C6%B0ng-l%C3%AA-quang-074199234/"
-            target="_blank"
-            rel="noreferrer"
-          >
-            <ion-icon name="logo-linkedin"></ion-icon>
-          </a>
+          <div className="nav-links">
+            <a
+              href="https://www.linkedin.com/in/h%C6%B0ng-l%C3%AA-quang-074199234/"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <ion-icon name="logo-linkedin"></ion-icon>
+            </a>
 
-          <a
-            href="https://www.facebook.com/hungle0301/"
-            target="_blank"
-            rel="noreferrer"
-          >
-            <ion-icon name="logo-facebook"></ion-icon>
-          </a>
+            <a
+              href="https://www.facebook.com/hungle0301/"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <ion-icon name="logo-facebook"></ion-icon>
+            </a>
 
-          <a
-            href="https://github.com/hungle311994"
-            target="_blank"
-            rel="noreferrer"
-          >
-            <ion-icon name="logo-github"></ion-icon>
-          </a>
+            <a
+              href="https://github.com/hungle311994"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <ion-icon name="logo-github"></ion-icon>
+            </a>
+          </div>
+        </div>
+
+        <div className="nav-options">
+          <button onClick={handleDropdowns}>
+            {isDropdowns && (
+              <span>
+                <ion-icon name="list-outline"></ion-icon>
+              </span>
+            )}
+
+            {!isDropdowns && (
+              <span>
+                <ion-icon name="close-outline"></ion-icon>
+              </span>
+            )}
+          </button>
         </div>
       </nav>
     </div>
